ad1b21642034de3034350d8f2315584907592fec
errabi ayoub <errabi.ayoub@gmail.com>
2022-07-31 16:15:28 +0100

    Create project structure

---
+++ ishop-modules/shipping-managment/src/main/java/com/errabi/ishop/ShippingManagmentApplication.java
@@ -0,0 +1,13 @@
+package com.errabi.ishop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShippingManagmentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShippingManagmentApplication.class, args);
+	}
+
+}
